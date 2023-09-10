0ca26dfcecd3aa286ecba8df1f83c47a58bdccc2
zevess <gazetdinovvlad04@gmail.com>
2023-09-10 12:59:26 +0300

    refactor

--- getWeather.js
+++ getWeather.js
@@ -1,5 +1,6 @@
 import { ELEMENTS } from "./elements.js";
 import { forecast } from "./forecast.js";
+import { weatherTitles } from "./elements.js";
 
 export async function getWeather() {
     if (event.key === 'Enter') {
@@ -18,7 +19,7 @@ export async function weatherNow(cityName) {
     let responce = await fetch(url);
     if (responce.ok) {
         let commits = await responce.json();
-        // console.log(commits);
+        console.log(commits);
 
         Array.from(ELEMENTS.CITY_TITLE_ALL).forEach(element => {
             element.textContent = cityName;
@@ -29,7 +30,6 @@ export async function weatherNow(cityName) {
         ELEMENTS.INPUT.value = '';
         forecast(cityName);
 
-        
     } else {
         if (responce.status === 404) {
             alert("Ошибка! Такого города не существует или написан не правильно!");
@@ -38,64 +38,50 @@ export async function weatherNow(cityName) {
     }
 }
 
-
 function setDetails(commits) {
     ELEMENTS.TEMPStat.textContent = "Температура: " + ELEMENTS.TEMP.textContent;
     ELEMENTS.FEELSLIKE.textContent = "Ощущается: " + `${(commits.main.feels_like - 273.15).toFixed(0)}` + String.fromCharCode(176);
     ELEMENTS.WEATHERStat.textContent = "Погода: "
     weatherTranslate(commits, ELEMENTS.WEATHERStat);
 
-    const sunrise = new Date(1000 * `${commits.sys.sunrise}`);
-    sunrise.toISOString;
-    const sunriseHours = sunrise.getHours().toString().padStart(2, '0');
-    const sunriseMinutes = sunrise.getMinutes().toString().padStart(2, '0');;
-    ELEMENTS.SUNRISE.textContent = "Восход: " + `${sunriseHours}:${sunriseMinutes}` + " (по МСК)";
-
-    const sunset = new Date(1000 * `${commits.sys.sunset}`);
-    sunrise.toISOString;
-    const sunsetHours = sunset.getHours().toString().padStart(2, '0');
-    const sunsetMinutes = sunset.getMinutes().toString().padStart(2, '0');
-    ELEMENTS.SUNSET.textContent = "Закат: " + `${sunsetHours}:${sunsetMinutes}` + " (по МСК)";
-
-    
     const weatherIMG = document.querySelector('.weather__img');
     const weatherIcon = commits.weather[0].icon;
     const weatherIconURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     weatherIMG.src = weatherIconURL;
-    // console.log(weatherIconURL);
+
+    setSunsetRise(commits, sunrise);
+    setSunsetRise(commits, sunset);
 }
 
+function setSunsetRise(commits, typeTime) {
+    if (typeTime == sunrise) {
+        const time = new Date(1000 * `${commits.sys.sunrise}`);
+        setTime(time, sunrise);
+    }
+    if (typeTime == sunset){
+        const time = new Date(1000 * `${commits.sys.sunset}`);
+        setTime(time, sunset);
+    }
+    
+}
+
+function setTime(time, typeTime){
+    time.toISOString;
+    const timeHours = time.getHours().toString().padStart(2, '0');
+    const timeMinutes = time.getMinutes().toString().padStart(2, '0');
+    if (typeTime == sunrise){
+        ELEMENTS.SUNRISE.textContent = "Восход: " + `${timeHours}:${timeMinutes}` + " (по МСК)";
+    } 
+    if (typeTime == sunset){
+        ELEMENTS.SUNSET.textContent = "Закат: " + `${timeHours}:${timeMinutes}` + " (по МСК)";
+    }
+
+}
 
 export function weatherTranslate(commits, textPlace) {
-    if (commits.weather[0].main === "Clouds") {
-        textPlace.textContent = textPlace.textContent + "Облачно";
-    } else if (commits.weather[0].main === "Thunderstorm") {
-        textPlace.textContent = textPlace.textContent + "Гроза";
-    } else if (commits.weather[0].main === "Drizzle") {
-        textPlace.textContent = textPlace.textContent + "Морось";
-    } else if (commits.weather[0].main === "Rain") {
-        textPlace.textContent = textPlace.textContent + "Дождь";
-    } else if (commits.weather[0].main === "Mist") {
-        textPlace.textContent = textPlace.textContent + "Туман";
-    } else if (commits.weather[0].main === "Snow") {
-        textPlace.textContent = textPlace.textContent + "Снег";
-    } else if (commits.weather[0].main === "Smoke") {
-        textPlace.textContent = textPlace.textContent + "Смог";
-    } else if (commits.weather[0].main === "Haze") {
-        textPlace.textContent = textPlace.textContent + "Мгла";
-    } else if (commits.weather[0].main === "Dust") {
-        textPlace.textContent = textPlace.textContent + "Пыль";
-    } else if (commits.weather[0].main === "Fog") {
-        textPlace.textContent = textPlace.textContent + "Густой туман";
-    } else if (commits.weather[0].main === "Sand") {
-        textPlace.textContent = textPlace.textContent + "Песчанная буря";
-    } else if (commits.weather[0].main === "Ash") {
-        textPlace.textContent = textPlace.textContent + "Вулканический пепел";
-    } else if (commits.weather[0].main === "Squall") {
-        textPlace.textContent = textPlace.textContent + "Шквал";
-    } else if (commits.weather[0].main === "Tornado") {
-        textPlace.textContent = textPlace.textContent + "Торнадо";
-    } else if (commits.weather[0].main === "Clear") {
-        textPlace.textContent = textPlace.textContent + "Ясно";
+    for (let key in weatherTitles) {
+        if (commits.weather[0].main === key) {
+            textPlace.textContent = textPlace.textContent + weatherTitles[key];
+        }
     }
 }
